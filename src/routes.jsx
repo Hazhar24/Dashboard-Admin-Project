4ad1513a5e65aed425eb12d4edb78dadc03b3fbf
hazhar24 <hazhar24ghaderi@gmail.com>
2023-05-06 12:21:32 +0430

    add routers full project

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,13 @@
-import Home from "./pages/Home"
+import Home from "./pages/Home/Home"
+import UserList from "./pages/Users/UserList"
+import NewUser from "./pages/NewUser/NewUser"
+import Products from "./pages/Products/Products"
 
 let Routes = [
-     {path:'/' , element:<Home />}
+     {path:'/' , element:<Home />},
+     {path:'/users' , element:<UserList />},
+     {path:'/NewUser' , element:<NewUser />},
+     {path:'/Products' , element:<Products />},
 ]
 
 
