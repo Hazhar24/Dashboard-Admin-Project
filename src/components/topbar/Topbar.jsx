fe64e59d8bfa2cd6081b213ba36204629fe60e84
hazhar24 <hazhar24ghaderi@gmail.com>
2023-05-07 13:24:59 +0430

    create TopBar project

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,11 +1,27 @@
 import { DataTopbar } from "../../data/DataTopbar";
+import {IoMdNotificationsOutline} from '@react-icons/all-files/io/IoMdNotificationsOutline'
+import {MdLanguage} from '@react-icons/all-files/md/MdLanguage'
+import {RiSettings5Line} from '@react-icons/all-files/ri/RiSettings5Line'
 
 export default function TopBar() {
   return (
-    <div>
+    <div className="topbar">
       <div>
-        <div className="btn">{DataTopbar.Topbar[0].name}</div>
-        <div></div>
+        <span className="btn">{DataTopbar.Topbar[0].name}</span>
+      </div>
+      <div className="topbarRight">
+        <div className="iconBar">
+          <IoMdNotificationsOutline />
+          <span className="spanTop">2</span>
+        </div>
+        <div className="iconBar">
+          <MdLanguage />
+          <span className="spanTop">2</span>
+        </div>
+        <div className="iconBar">
+          <RiSettings5Line />
+        </div>
+          <img className="imageBar" src="image/IMG2.jpg" alt="" />
       </div>
     </div>
   );
