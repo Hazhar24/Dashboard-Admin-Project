637e90e0fd4d367655f87c3d5731c596595aca5a
hazhar24 <hazhar24ghaderi@gmail.com>
2023-05-07 13:34:25 +0430

    update name class styles

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -5,23 +5,23 @@ import {RiSettings5Line} from '@react-icons/all-files/ri/RiSettings5Line'
 
 export default function TopBar() {
   return (
-    <div className="topbar">
+    <div className="TopBar">
       <div>
-        <span className="btn">{DataTopbar.Topbar[0].name}</span>
+        <span className="TopBarLogo">{DataTopbar.Topbar[0].name}</span>
       </div>
       <div className="topbarRight">
-        <div className="iconBar">
+        <div className="TopBarIcon">
           <IoMdNotificationsOutline />
-          <span className="spanTop">2</span>
+          <span className="TopBarSpan">2</span>
         </div>
-        <div className="iconBar">
+        <div className="TopBarIcon">
           <MdLanguage />
-          <span className="spanTop">2</span>
+          <span className="TopBarSpan">2</span>
         </div>
-        <div className="iconBar">
+        <div className="TopBarIcon">
           <RiSettings5Line />
         </div>
-          <img className="imageBar" src="image/IMG2.jpg" alt="" />
+          <img className="TopBarImage" src="image/IMG2.jpg" alt="" />
       </div>
     </div>
   );
