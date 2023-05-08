cf45073ad9b734edd9d8a1b01c1a6ed830bc9518
hazhar24 <hazhar24ghaderi@gmail.com>
2023-05-08 14:12:28 +0430

    add data for sidbar

---
+++ src/data/DataSideBar.jsx
@@ -0,0 +1,22 @@
+export const DataSideBar = {
+     SideBar:[
+          {name:'Dashboard' },
+          {name:'Home' },
+          {name:'Analytics' },
+          {name:'Sales' },
+          {name:'Quick Menu' },
+          {name:'Users' },
+          {name:'New User' },
+          {name:'Products' },
+          {name:'Transactions' },
+          {name:'Reports' },
+          {name:'Notfications' },
+          {name:'Mail' },
+          {name:'FeedBack' },
+          {name:'Massages' },
+          {name:'Staff' },
+          {name:'Manage' },
+          {name:'Analytics' },
+          {name:'Reports' },
+     ]
+}
