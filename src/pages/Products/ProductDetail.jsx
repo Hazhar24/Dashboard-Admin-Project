82907173af00b8cd3aa4ba6a07f96bee6fe075fe
hazhar24 <hazhar24ghaderi@gmail.com>
2023-05-22 13:54:46 +0430

    Finishing the product details section

--- src/pages/Products/ProductDetail.jsx
+++ src/pages/Products/ProductDetail.jsx
@@ -4,11 +4,16 @@ import { DataProducts } from "@/datas";
 
 function ProductDetail() {
   const params = useParams();
-  console.log(typeof params);
-
-  {DataProducts.find((item) => item.id === params.postID)}
-  console.log(DataProducts);
-
-  return <div>main post : </div>;
+  const postId = parseInt(params.postID); // Convert the ID string to a number
+  const product = DataProducts.find((item) => item.id === postId);
+  return (
+    <div className="Details">
+      <p className="NameItem">Name : {product ? product.Name : "Not found"}</p>
+      <p className="NameItem">Price : {product ? product.Price : "Not found"}</p>
+      <p className="NameItem">{product ? product.Detail : "Not found"}</p>
+      <p className="AboutItem">{product ? product.About : "Not found"}</p>
+    </div>
+  )
 }
+
 export default ProductDetail;
